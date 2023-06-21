b0647f0bd52d834b807a3b294a30db09d28c80c1
AndrasSzep <60442692+AndrasSzep@users.noreply.github.com>
2023-06-21 22:04:57 +0100

    Update config.h

--- config.h
+++ config.h
@@ -3,8 +3,8 @@
 #define ENVSENSOR       //environmental sensors connected
 
 char storedWIFI[80],storedPWD[80];
-const char* myWIFI  = "1114297_Trooli.uk";
-const char* myPWD   = "aZRkOKpL";
+const char* myWIFI  = "your_ssid";
+const char* myPWD   = "your_passw";
 const char*   servername  = "nmea0183";     //nDNS servername - http://servername.local
 unsigned int UDPPort = 10110; // 10110 is the default NMEA0183 port#
 
