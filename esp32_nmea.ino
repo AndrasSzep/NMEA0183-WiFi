b9f48ccaefbb785de14a43601306de099ff454fe
AndrasSzep <60442692+AndrasSzep@users.noreply.github.com>
2023-06-21 21:36:56 +0100

    Update esp32_nmea.ino

--- esp32_nmea.ino
+++ esp32_nmea.ino
@@ -1,12 +1,5 @@
 /* 
-  Rui Santos
-  Complete project details at https://RandomNerdTutorials.com/esp32-web-server-websocket-sliders/
-  
-  Permission is hereby granted, free of charge, to any person obtaining a copy
-  of this software and associated documentation files.
-  
-  The above copyright notice and this permission notice shall be included in all
-  copies or substantial portions of the Software.
+
 */
 
 #include <Arduino.h>
